e85b957e233751acccd03ec87cbc9cadc29d3d79
Anush Arakelyan <anush_a@instigatemobile.com>
2015-12-02 18:05:09 +0400

    Write javascript functions. i Please enter the commit message for your changes. Lines starting

---
+++ main.js
@@ -0,0 +1,20 @@
+function clickButton() {
+	alert('uraaaaaaaaaa');
+}
+
+function myFunction() {
+	var str = "tparan";
+	alert(str.length);
+	str = str[2] + str[3] + str[4];
+	alert(str.toUpperCase());
+}
+
+function upercase() {
+	/*var str = "were4r esresr ere ido";
+	alert(str.indexOf("ido"))*/;
+	var blablabla = {['name']: "Ani"};
+	alert(blablabla.lala === undefined);
+	alert(blablabla.name === undefined);
+	var message =  'Hello!', age = 25; user = 'John';
+	alert( message + age + user) ;
+}
